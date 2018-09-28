9e025643fc36d8fcdfab9f4a6fd426f7999190ae
bsaja <saja@w11k.de>
2018-09-28 19:21:35 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ons from 'onsenui/esm';
+import 'onsenui/esm/elements/ons-page';
+import 'onsenui/esm/elements/ons-toolbar';
+import 'onsenui/esm/elements/ons-toolbar-button';
+import 'onsenui/esm/elements/ons-icon';
