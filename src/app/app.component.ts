67b16cba5f7fadd256eded2b31b27f92945ef341
bsaja <saja@w11k.de>
2018-11-13 09:23:24 +0100

    Remove cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,18 +18,10 @@ export class AppComponent {
     }
   ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
   }
 }
