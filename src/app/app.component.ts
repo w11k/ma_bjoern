406f69d149d9108dab2834a3268b4d21114e5e42
bsaja <saja@w11k.de>
2018-11-16 15:12:35 +0100

    Add sidenav and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-fw';
+export class AppComponent implements OnDestroy {
+  private readonly _mobileQueryListener: () => void;
+  public mobileQuery: MediaQueryList;
+  public appPages = [
+    {
+      title: 'Todos',
+      url: '.',
+      icon: 'list'
+    },
+    {
+      title: 'Settings',
+      url: '.',
+      icon: 'settings'
+    },
+    {
+      title: 'About',
+      url: '.',
+      icon: 'information-circle'
+    }
+  ];
+
+  constructor(public changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
+  ]
+})
+export class MaterialModule {
+}
