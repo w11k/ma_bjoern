7e0c288b0111f4e60a629aae3e9f599cbad7943f
bsaja <saja@w11k.de>
2018-11-22 10:38:38 +0100

    Rename project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  storeName: 'angular-fw'
+  storeName: 'angular-wc'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  storeName: 'angular-fw'
+  storeName: 'angular-wc'
 };
 
 /*
