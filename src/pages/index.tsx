c111693eebc562029ef5a94fcbd45b65e330911a
bsaja <saja@w11k.de>
2018-12-04 17:08:19 +0100

    Add app drawer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,181 @@
-import * as React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Typography from '@material-ui/core/Typography';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import {PortalProps} from '@material-ui/core/Portal';
+import {Theme, WithStyles, withStyles, WithTheme} from '@material-ui/core/styles';
 import createStyles from '@material-ui/core/styles/createStyles';
-import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import React from 'react';
 import withRoot from '../withRoot';
 
-const styles = (theme: Theme) =>
-  createStyles({
-    root: {
-      textAlign: 'center',
-      paddingTop: theme.spacing.unit * 20,
-    },
-  });
+const drawerWidth = 240;
 
+const styles = (theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex'
+        },
+        drawer: {
+            [theme.breakpoints.up('sm')]: {
+                width: drawerWidth,
+                flexShrink: 0
+            }
+        },
+        appBar: {
+            marginLeft: drawerWidth,
+            [theme.breakpoints.up('sm')]: {
+                width: `calc(100% - ${drawerWidth}px)`
+            }
+        },
+        menuButton: {
+            marginRight: 20,
+            [theme.breakpoints.up('sm')]: {
+                display: 'none'
+            }
+        },
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: drawerWidth
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing.unit * 3
+        }
+    });
 type State = {
-  open: boolean;
+    mobileOpen: boolean;
 };
 
-class Index extends React.Component<WithStyles<typeof styles>, State> {
-  state = {
-    open: false,
-  };
+class ResponsiveDrawer extends React.Component<ResponsiveDrawerProps, State> {
+    state: State = {
+        mobileOpen: false
+    };
 
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
+    handleDrawerToggle = () => {
+        this.setState((state: State) => ({mobileOpen: !state.mobileOpen}));
+    };
 
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
-  };
+    render() {
+        const {classes, theme} = this.props;
 
-  render() {
-    return (
-      <div className={this.props.classes.root}>
-        <Dialog open={this.state.open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography variant="h4" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          example project
-        </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
-      </div>
-    );
-  }
+        const drawer = (
+            <div>
+                <div className={classes.toolbar}/>
+                <Divider/>
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        );
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline/>
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Responsive drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer}>
+                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                    <Hidden smUp implementation="css">
+                        <Drawer
+                            container={this.props.container}
+                            variant="temporary"
+                            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                            open={this.state.mobileOpen}
+                            onClose={this.handleDrawerToggle}
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                            ModalProps={{
+                                keepMounted: true // Better open performance on mobile.
+                            }}
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                </nav>
+                <main className={classes.content}>
+                    <div className={classes.toolbar}/>
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+                        elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                        hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+                        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+                        Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+                        viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+                        Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+                        at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+                        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+                    </Typography>
+                    <Typography paragraph>
+                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
+                        sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
+                        In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                        sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
+                        viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+                        ultrices sagittis orci a.
+                    </Typography>
+                </main>
+            </div>
+        );
+    }
 }
 
-export default withRoot(withStyles(styles)(Index));
+type ResponsiveDrawerProps = WithStyles<typeof styles> & WithTheme & {
+    container?: PortalProps['container'],
+};
+
+export default withRoot(withStyles(styles, {withTheme: true})(ResponsiveDrawer));
--- src/withRoot.tsx
+++ src/withRoot.tsx
@@ -1,35 +1,34 @@
-import * as React from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import {pink} from '@material-ui/core/colors';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import * as React from 'react';
 
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: green,
-  },
-  typography: {
-    useNextVariants: true,
-  },
+    palette: {
+        primary: {main: '#0129c7'},
+        secondary: pink
+    },
+    typography: {
+        useNextVariants: true
+    }
 });
 
 function withRoot<P>(Component: React.ComponentType<P>) {
-  function WithRoot(props: P) {
-    // MuiThemeProvider makes the theme available down the React tree
-    // thanks to React context.
-    return (
-      <MuiThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...props} />
-      </MuiThemeProvider>
-    );
-  }
+    function WithRoot(props: P) {
+        // MuiThemeProvider makes the theme available down the React tree
+        // thanks to React context.
+        return (
+            <MuiThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
+                <Component {...props} />
+            </MuiThemeProvider>
+        );
+    }
 
-  return WithRoot;
+    return WithRoot;
 }
 
 export default withRoot;
