7e93f56405ef59378ca7cd5fe90a44ba591c8d93
bsaja <saja@w11k.de>
2018-12-07 11:28:10 +0100

    Add badges

--- src/components/pages/TabsPage.jsx
+++ src/components/pages/TabsPage.jsx
@@ -1,9 +1,10 @@
-import {Fab, Icon, Link, Navbar, NavLeft, NavTitle, Page, Tab, Tabs, Toolbar} from 'framework7-react';
+import {Badge, Fab, Icon, Link, Navbar, NavLeft, NavTitle, Page, Tab, Tabs, Toolbar} from 'framework7-react';
 import React from 'react';
+import {view} from 'react-easy-state';
 import model from '../../model';
 import {ListTypes} from '../../routes';
 
-export default class TabsPage extends React.Component {
+class TabsPage extends React.Component {
     constructor(props) {
         super(props);
         this.model = model;
@@ -17,11 +18,27 @@ export default class TabsPage extends React.Component {
                 </NavLeft>
                 <NavTitle>Todos</NavTitle>
             </Navbar>
-            <Toolbar tabbar bgColor="primary">
-                <Link tabLink routeTabId={ListTypes.ALL} href="/todos/all" tabLinkActive>All</Link>
-                <Link tabLink routeTabId={ListTypes.ACTIVE} href="/todos/active">Active</Link>
-                <Link tabLink routeTabId={ListTypes.COMPLETED} href="/todos/completed">Completed</Link>
+            <Toolbar tabbar labels bgColor="primary">
+                <Link tabLink routeTabId={ListTypes.ALL} href="/todos/all" tabLinkActive>
+                    <Icon ios="f7:list" md="material:list">
+                        <Badge color="red">{this.model.getCount().total.toString()}</Badge>
+                    </Icon>
+                    <span className="tabbar-label">All</span>
+                </Link>
+                <Link tabLink routeTabId={ListTypes.ACTIVE} href="/todos/active">
+                    <Icon ios="f7:circle" md="material:check_box_outline_blank">
+                        <Badge color="red">{this.model.getCount().active.toString()}</Badge>
+                    </Icon>
+                    <span className="tabbar-label">Active</span>
+                </Link>
+                <Link tabLink routeTabId={ListTypes.COMPLETED} href="/todos/completed">
+                    <Icon className="icon-fill" ios="f7:check_round_fill" md="material:check_box">
+                        <Badge color="red">{this.model.getCount().completed.toString()}</Badge>
+                    </Icon>
+                    <span className="tabbar-label">Completed</span>
+                </Link>
             </Toolbar>
+
             <Tabs routable>
                 <Tab id={ListTypes.ALL} tabActive/>
                 <Tab id={ListTypes.ACTIVE}/>
@@ -44,3 +61,5 @@ export default class TabsPage extends React.Component {
         input.focus();
     }
 }
+
+export default view(TabsPage);
