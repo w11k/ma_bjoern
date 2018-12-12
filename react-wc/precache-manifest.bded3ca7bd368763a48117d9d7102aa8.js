b509dd06fa2aa1e30da6f618f6a8155ed3118c7b
bsaja <saja@w11k.de>
2018-12-12 14:40:41 +0100

    Updates

---
+++ react-wc/precache-manifest.bded3ca7bd368763a48117d9d7102aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d376e994630383bf8f07",
+    "url": "/ma_bjoern/react-wc/static/js/runtime~main.d376e994.js"
+  },
+  {
+    "revision": "64373535a2c7a8ae3636",
+    "url": "/ma_bjoern/react-wc/static/js/main.64373535.chunk.js"
+  },
+  {
+    "revision": "8d19c3bdbbac1cdd8cfa",
+    "url": "/ma_bjoern/react-wc/static/js/1.8d19c3bd.chunk.js"
+  },
+  {
+    "revision": "64373535a2c7a8ae3636",
+    "url": "/ma_bjoern/react-wc/static/css/main.cc2377f9.chunk.css"
+  },
+  {
+    "revision": "e17bb35f9e11f58d6a2d7557d7584322",
+    "url": "/ma_bjoern/react-wc/index.html"
+  }
+];
--- react-wc/service-worker.js
+++ react-wc/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ma_bjoern/react-wc/precache-manifest.e0be5d0faa568f72ef20fd708e05950e.js"
+  "/ma_bjoern/react-wc/precache-manifest.bded3ca7bd368763a48117d9d7102aa8.js"
 );
 
 workbox.clientsClaim();
