9bc47b9f2764bf9e8f7fc007783dc0975dd5b6d4
bsaja <saja@w11k.de>
2018-12-12 15:24:11 +0100

    Updates

---
+++ react-nui/precache-manifest.68861bf1c57c60e29d9c15f66122f4ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/ma_bjoern/react-nui/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "5a0f09c63ef58e7f3b3d",
+    "url": "/ma_bjoern/react-nui/static/css/main.b218b116.chunk.css"
+  },
+  {
+    "revision": "a21cd880069ba3c588bc1c0e34190c13",
+    "url": "/ma_bjoern/react-nui/static/media/Framework7Icons-Regular.a21cd880.ttf"
+  },
+  {
+    "revision": "7eec633b90f0458a510d",
+    "url": "/ma_bjoern/react-nui/static/js/1.7eec633b.chunk.js"
+  },
+  {
+    "revision": "5ce2ec60666594fcd519",
+    "url": "/ma_bjoern/react-nui/static/js/runtime~main.5ce2ec60.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/ma_bjoern/react-nui/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/ma_bjoern/react-nui/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "5a0f09c63ef58e7f3b3d",
+    "url": "/ma_bjoern/react-nui/static/js/main.5a0f09c6.chunk.js"
+  },
+  {
+    "revision": "df27d9863abee94e1012e147536ea8f3",
+    "url": "/ma_bjoern/react-nui/static/media/Framework7Icons-Regular.df27d986.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/ma_bjoern/react-nui/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "ea912ff15c9f2a41ac98a723471afef0",
+    "url": "/ma_bjoern/react-nui/static/media/Framework7Icons-Regular.ea912ff1.woff2"
+  },
+  {
+    "revision": "cf0ac81df2df1cdcb5c6838d34837d68",
+    "url": "/ma_bjoern/react-nui/static/media/Framework7Icons-Regular.cf0ac81d.woff"
+  },
+  {
+    "revision": "7eec633b90f0458a510d",
+    "url": "/ma_bjoern/react-nui/static/css/1.20f3ea26.chunk.css"
+  },
+  {
+    "revision": "608d652b1da3677bba63c1dd87a1e90d",
+    "url": "/ma_bjoern/react-nui/index.html"
+  }
+];
