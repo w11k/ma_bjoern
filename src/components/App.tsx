cb173c294d78181ee4ab52743cda0502461db11b
bsaja <saja@w11k.de>
2018-12-12 15:21:33 +0100

    Fix trailing slash

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import {createStyles, CssBaseline, Theme, withStyles, WithStyles} from '@material-ui/core';
 import React from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import {pages} from '../constants';
 import {AppComponentState} from '../typings';
 import withRoot from '../withRoot';
@@ -42,27 +42,34 @@ class AppComponent extends React.Component<WithStyles<typeof styles>, AppCompone
     render() {
         const {classes} = this.props;
         return (
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <div className={classes.root}>
-                    <CssBaseline/>
-                    <AppBar toggleDrawer={this.handleDrawerToggle} title={this.state.title}/>
-                    <Drawer mobileOpen={this.state.mobileOpen} closeDrawer={this.handleDrawerClose}/>
-                    <main className={classes.content}>
-                        <div className={classes.toolbar}/>
-                        <Switch>
-                            <Route path={pages.todos.url} render={(props) => <Tabs {...props}
-                                                                                   title={pages.todos.title}
-                                                                                   setTitle={this.setTitle}/>}/>
-                            <Route path={pages.settings.url} render={(props) => <Settings {...props}
-                                                                                          title={pages.settings.title}
-                                                                                          setTitle={this.setTitle}/>}/>
-                            <Route path={pages.about.url} render={(props) => <About {...props}
-                                                                                    title={pages.about.title}
-                                                                                    setTitle={this.setTitle}/>}/>
-                            <Redirect to={pages.todos.tabs.all.url}/>
-                        </Switch>
-                    </main>
-                </div>
+            <BrowserRouter basename="/">
+                <Switch>
+                    <Route strict path={process.env.PUBLIC_URL + '/'}>
+                        <HashRouter>
+                            <div className={classes.root}>
+                                <CssBaseline/>
+                                <AppBar toggleDrawer={this.handleDrawerToggle} title={this.state.title}/>
+                                <Drawer mobileOpen={this.state.mobileOpen} closeDrawer={this.handleDrawerClose}/>
+                                <main className={classes.content}>
+                                    <div className={classes.toolbar}/>
+                                    <Switch>
+                                        <Route path={pages.todos.url} render={(props) => <Tabs {...props}
+                                                                                               title={pages.todos.title}
+                                                                                               setTitle={this.setTitle}/>}/>
+                                        <Route path={pages.settings.url} render={(props) => <Settings {...props}
+                                                                                                      title={pages.settings.title}
+                                                                                                      setTitle={this.setTitle}/>}/>
+                                        <Route path={pages.about.url} render={(props) => <About {...props}
+                                                                                                title={pages.about.title}
+                                                                                                setTitle={this.setTitle}/>}/>
+                                        <Redirect to={pages.todos.tabs.all.url}/>
+                                    </Switch>
+                                </main>
+                            </div>
+                        </HashRouter>
+                    </Route>
+                    <Redirect to={process.env.PUBLIC_URL + '/'}/>
+                </Switch>
             </BrowserRouter>
         );
     }
