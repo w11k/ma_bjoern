b9aa167c6c45d07817a88044bc9af60e0668eccc
PeterBurner <the_burner@gmx.net>
2019-01-06 14:56:16 +0100

    Add badges

--- app/src/main/java/de/w11k/bsaja/todo/FragmentTabs.java
+++ app/src/main/java/de/w11k/bsaja/todo/FragmentTabs.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -18,26 +17,32 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
+import com.rahimlis.badgedtablayout.BadgedTabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import de.w11k.bsaja.todo.database.Item;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class FragmentTabs extends Fragment implements FragmentList.OnListInteractionListener, FragmentBottomSheet.OnBottomSheetInteractionListener {
     private static final String TAG = FragmentTabs.class.getSimpleName();
-    private final CompositeDisposable[] mDisposable = {
+    private final CompositeDisposable[] mDisposableTabContent = {
             new CompositeDisposable(),
             new CompositeDisposable(),
             new CompositeDisposable()
     };
+    private Disposable mDisposableGetCount;
+    private Disposable mDisposableGetItem;
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private ViewModelFactory mViewModelFactory;
     private TodoViewModel mViewModel;
+    private BadgedTabLayout mTabLayout;
 
     public FragmentTabs() {
     }
@@ -50,9 +55,14 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
         mSectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
         mViewPager = view.findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        TabLayout tabLayout = view.findViewById(R.id.tabs);
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        mTabLayout = view.findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.setIcon(0, R.drawable.ic_list_white_24dp);
+        mTabLayout.setIcon(1, R.drawable.ic_check_box_outline_white_24dp);
+        mTabLayout.setIcon(2, R.drawable.ic_check_box_white_24dp);
+        mTabLayout.setBadgeText(0, "0");
+        mTabLayout.setBadgeText(1, "0");
+        mTabLayout.setBadgeText(2, "0");
 
         FloatingActionButton fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -84,7 +94,7 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
 
     @Override
     public void onListViewStart(final FragmentList fragment) {
-        mDisposable[fragment.getListType().getValue()].add(mViewModel.getItems()
+        mDisposableTabContent[fragment.getListType().getValue()].add(mViewModel.getItems()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<List<Item>>() {
@@ -97,7 +107,7 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
                 }, new Consumer<Throwable>() {
                     @Override
                     public void accept(Throwable throwable) throws Exception {
-                        Log.e(TAG, "Unable to update username", throwable);
+                        Log.e(TAG, "Unable to get tab content", throwable);
                     }
                 }));
     }
@@ -114,7 +124,7 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
 
     @Override
     public void onListViewStop(FragmentList fragment) {
-        mDisposable[fragment.getListType().getValue()].clear();
+        mDisposableTabContent[fragment.getListType().getValue()].clear();
     }
 
     public void onAddListItem() {
@@ -175,7 +185,7 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
     public void onSelectSheetItem(int position, String itemId) {
         switch (position) {
             case 0:
-                mViewModel.getItemById(itemId)
+                mDisposableGetItem = mViewModel.getItemById(itemId)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(new Consumer<Item>() {
@@ -201,6 +211,41 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
         }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mDisposableGetCount = mViewModel.getCount()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        new Consumer<TodoViewModel.Count>() {
+                            @Override
+                            public void accept(TodoViewModel.Count count) throws Exception {
+                                mTabLayout.setBadgeText(0, String.valueOf(count.getTotal()));
+                                mTabLayout.setBadgeText(1, String.valueOf(count.getActive()));
+                                mTabLayout.setBadgeText(2, String.valueOf(count.getCompleted()));
+                            }
+                        },
+                        new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                Log.e(TAG, "Unable to get count", throwable);
+                            }
+                        }
+                );
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mDisposableGetItem != null && !mDisposableGetItem.isDisposed()) {
+            mDisposableGetItem.dispose();
+        }
+        if (mDisposableGetCount != null && !mDisposableGetCount.isDisposed()) {
+            mDisposableGetCount.dispose();
+        }
+    }
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -215,6 +260,20 @@ public class FragmentTabs extends Fragment implements FragmentList.OnListInterac
         @Override
         public int getCount() {
             return 3;
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "All";
+                case 1:
+                    return "Active";
+                case 2:
+                    return "Completed";
+            }
+            return null;
         }
     }
 }
