d37a751b07fb865e214af41180ac6e906059c57b
bsaja <saja@w11k.de>
2019-01-08 15:28:13 +0100

    Add splash screen

---
+++ app/src/main/java/de/w11k/bsaja/todo/SplashActivity.java
@@ -0,0 +1,16 @@
+package de.w11k.bsaja.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, ActivityMain.class));
+        // close splash activity
+        finish();
+    }
+}
