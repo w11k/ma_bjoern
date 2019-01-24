4844a4586ff98282c0da1cac1b9cfd8a60a43732
bsaja <saja@w11k.de>
2019-01-24 13:47:16 +0100

    Updates

--- react-wc/precache-manifest.91ed798f7ba963be5844424f4f291747.js
+++ react-wc/precache-manifest.91ed798f7ba963be5844424f4f291747.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ma_bjoern/react-wc/static/js/runtime~main.d376e994.js"
   },
   {
-    "revision": "e841b2f78035a7075af3",
-    "url": "/ma_bjoern/react-wc/static/js/main.e841b2f7.chunk.js"
+    "revision": "f39102afb47cd80464e9",
+    "url": "/ma_bjoern/react-wc/static/js/main.f39102af.chunk.js"
   },
   {
     "revision": "8d19c3bdbbac1cdd8cfa",
     "url": "/ma_bjoern/react-wc/static/js/1.8d19c3bd.chunk.js"
   },
   {
-    "revision": "e841b2f78035a7075af3",
-    "url": "/ma_bjoern/react-wc/static/css/main.d2c34d55.chunk.css"
+    "revision": "f39102afb47cd80464e9",
+    "url": "/ma_bjoern/react-wc/static/css/main.27750712.chunk.css"
   },
   {
-    "revision": "1b56699c7e0b8d91c9ce8698ef6f20fd",
+    "revision": "cd2a050ab5df2fe4a4eda9ae3d0fccf2",
     "url": "/ma_bjoern/react-wc/index.html"
   }
 ];
